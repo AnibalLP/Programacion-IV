ccdc3ed03952771fedbd3574bba458ef408cb151
Luis Hernandez <sarco_luis@hotmail.com>
2021-02-03 13:51:54 -0600

    Terminacion de la app de registro de productos vue.js + bootstrap + WebSQL

--- main.js
+++ main.js
@@ -1,11 +1,18 @@
 var miDBProductos = openDatabase('dbProductos','1.0','Aplicacion de Productos',5*1024*1024);
-window.id = 0;
+var generarIdUnicoDesdeFecha=()=>{
+    let fecha = new Date();//03/02/2021
+    return Math.floor(fecha.getTime()/1000).toString(16);
+};
 if(!miDBProductos){
     alert("Elnavegador no soporta Web SQL");
 }
 var appVue = new Vue({
     el:'#appProductos',
     data:{
+        accion : 'nuevo',
+        msg    : '',
+        status : false,
+        error : false,
         producto:{
             idProducto  : 0,
             codigo      : '',
@@ -21,13 +28,30 @@ var appVue = new Vue({
             /**
              * BD Web SQL
              */
+            let sql = '';
+            if( this.accion=='nuevo' ){
+                this.producto.idProducto= generarIdUnicoDesdeFecha();
+                sql = 'INSERT INTO productos(codigo,descripcion,precio,img,idProducto) VALUES(?,?,?,?,?)';
+            } else if(this.accion=='modificar'){
+                sql = 'UPDATE productos SET codigo=?,descripcion=?,precio=?,img=? WHERE idProducto=?';
+            }
             miDBProductos.transaction(tran=>{
-                tran.executeSql('INSERT INTO productos(idProducto,codigo,descripcion,precio,img) VALUES(?,?,?,?,?) ',
-                    [++id,this.producto.codigo,this.producto.descripcion,this.producto.precio, this.producto.img]);
+                tran.executeSql(sql,
+                    [this.producto.codigo,this.producto.descripcion,this.producto.precio, this.producto.img,this.producto.idProducto]);
                 this.obtenerProductos();
                 this.limpiar();
+                this.status = true;
+                this.msg = 'Registro almacenado con exito.';
+                this.error = false;
+
+                setTimeout(()=>{
+                    this.status=false;
+                    this.msg = '';
+                }, 3000)
             }, err=>{
-                console.log( err );
+                this.status = true;
+                this.msg = err.message;
+                this.error = true;
             });
         },
         obtenerImg(e){
@@ -50,12 +74,26 @@ var appVue = new Vue({
         },
         mostrarProducto(pro){
             this.producto = pro;
+            this.accion='modificar';
         },
         limpiar(){
+            this.accion='nuevo';
+            this.producto.idProducto='';
             this.producto.codigo='';
             this.producto.descripcion='';
             this.producto.precio='';
             this.producto.img='';
+        },
+        eliminarProducto(pro){
+            if( confirm(`Esta seguro que desea eliminar el producto:  ${pro.descripcion}`) ){
+                miDBProductos.transaction(tran=>{
+                    tran.executeSql('DELETE FROM productos WHERE idProducto=?',
+                        [pro.idProducto]);
+                    this.obtenerProductos();
+                }, err=>{
+                    console.log( err );
+                });
+            }
         }
     },
     created(){
