6dfd4a44dce17b252894b9b5560a5d46944a38a7
Luis Hernandez <sarco_luis@hotmail.com>
2021-02-25 14:31:57 -0600

    modificaciones hechas a la base de datos

--- db_sistema_facturacion.sql
+++ db_sistema_facturacion.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Servidor: 127.0.0.1
--- Tiempo de generación: 24-02-2021 a las 21:25:41
+-- Tiempo de generación: 25-02-2021 a las 21:31:09
 -- Versión del servidor: 10.4.14-MariaDB
 -- Versión de PHP: 7.2.33
 
@@ -14,6 +14,8 @@ SET time_zone = "+00:00";
 --
 -- Base de datos: `db_sistema_facturacion`
 --
+CREATE DATABASE IF NOT EXISTS `db_sistema_facturacion` DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci;
+USE `db_sistema_facturacion`;
 
 -- --------------------------------------------------------
 
@@ -28,6 +30,16 @@ CREATE TABLE `categorias` (
   `idC` char(10) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
+--
+-- Volcado de datos para la tabla `categorias`
+--
+
+INSERT INTO `categorias` (`idCategoria`, `codigo`, `descripcion`, `idC`) VALUES
+(6, '01', 'Teclado Logic Tech', '6037ff80'),
+(7, '02', 'Mouse Logic Tech', '6037ff97'),
+(8, '03', 'CPU INTEL', '6037ffa4'),
+(9, '04', 'CPU AMD', '6037ffb1');
+
 -- --------------------------------------------------------
 
 --
@@ -36,12 +48,19 @@ CREATE TABLE `categorias` (
 
 CREATE TABLE `productos` (
   `idProducto` int(10) NOT NULL,
-  `idCategoria` int(10) NOT NULL,
+  `idC` char(10) NOT NULL,
   `codigo` char(10) NOT NULL,
   `descripcion` char(75) NOT NULL,
   `precio` decimal(6,2) NOT NULL,
   `idP` char(10) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Volcado de datos para la tabla `productos`
+--
+
+INSERT INTO `productos` (`idProducto`, `idC`, `codigo`, `descripcion`, `precio`, `idP`) VALUES
+(2, '6037ff80', 'TE01', 'Teclado Inalambrico', '12.50', '6038081c');
 
 --
 -- Índices para tablas volcadas
@@ -51,14 +70,15 @@ CREATE TABLE `productos` (
 -- Indices de la tabla `categorias`
 --
 ALTER TABLE `categorias`
-  ADD PRIMARY KEY (`idCategoria`);
+  ADD PRIMARY KEY (`idCategoria`),
+  ADD KEY `idC` (`idC`);
 
 --
 -- Indices de la tabla `productos`
 --
 ALTER TABLE `productos`
   ADD PRIMARY KEY (`idProducto`),
-  ADD KEY `idCategoria` (`idCategoria`);
+  ADD KEY `idCategoria` (`idC`);
 
 --
 -- AUTO_INCREMENT de las tablas volcadas
@@ -68,13 +88,13 @@ ALTER TABLE `productos`
 -- AUTO_INCREMENT de la tabla `categorias`
 --
 ALTER TABLE `categorias`
-  MODIFY `idCategoria` int(10) NOT NULL AUTO_INCREMENT;
+  MODIFY `idCategoria` int(10) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=11;
 
 --
 -- AUTO_INCREMENT de la tabla `productos`
 --
 ALTER TABLE `productos`
-  MODIFY `idProducto` int(10) NOT NULL AUTO_INCREMENT;
+  MODIFY `idProducto` int(10) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
 
 --
 -- Restricciones para tablas volcadas
@@ -84,5 +104,5 @@ ALTER TABLE `productos`
 -- Filtros para la tabla `productos`
 --
 ALTER TABLE `productos`
-  ADD CONSTRAINT `productos_ibfk_1` FOREIGN KEY (`idCategoria`) REFERENCES `categorias` (`idCategoria`) ON DELETE CASCADE ON UPDATE CASCADE;
+  ADD CONSTRAINT `productos_ibfk_1` FOREIGN KEY (`idC`) REFERENCES `categorias` (`idC`) ON DELETE CASCADE ON UPDATE CASCADE;
 COMMIT;
